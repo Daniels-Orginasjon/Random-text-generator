8da961013ab2bb0d2f516c1ca26653f175a974dd
Gøran Knutsen <gorknu19@nfk.no>
2022-08-30 08:35:15 +0200

    added more pages

---
+++ pages/api/openai/pickups.ts
@@ -0,0 +1,50 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { openai, openAi } from '../../../lib/server/openai';
+import { CreateCompletionResponse } from 'openai';
+import nc from 'next-connect';
+
+export type ResponseData = {
+  response: CreateCompletionResponse;
+};
+
+interface PromptApiRequest extends NextApiRequest {
+  query: {
+    prompt: string;
+  };
+}
+const shuffleArray = (arr: any[]) => arr.sort(() => 0.5 - Math.random());
+
+const handler = nc({
+  onError: (err, req: NextApiRequest, res: NextApiResponse, next) => {
+    res.status(500).send('Server error');
+  },
+  onNoMatch: (req: NextApiRequest, res: NextApiResponse) => {
+    res.status(404).send('Not found!');
+  },
+});
+
+handler.get(
+  async (req: PromptApiRequest, res: NextApiResponse<ResponseData>) => {
+    let ai = new openAi({
+      openAi: openai,
+      prompt: '',
+      settings: {
+        model: 'text-davinci-002',
+        temperature: 1,
+        max_tokens: 256,
+        top_p: 1,
+        frequency_penalty: 0,
+        presence_penalty: 0,
+        stop: ['"'],
+      },
+    });
+    ai._addExample('Create a random pick-up line');
+    ai._addExample('This is a pick-up line: "');
+
+    let call = await ai.generate();
+    res.status(200).json({ response: call.data });
+  },
+);
+
+export default handler;
---
+++ pages/recipes.tsx
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+import { ResponseData } from '../pages/api/openai/pickups';
+let WEB_URL = "http://localhost:3000/"
+
+
+export default function recipes() {
+    const [recipe, setRecipe] = useState("");
+    const [loading, setLoading] = useState(false);
+    //const [pickupArray, setPickupArray] = useState([]);
+    const handleClick = () => {
+          if (loading === true) return false;
+        setLoading(true);
+        
+          fetch(WEB_URL + "api/openai/recipes")
+      .then((res) => 
+        res.json()
+    )
+      .then((data: ResponseData) => {
+        if (data.response.choices !== undefined && data.response.choices.length > 0 && typeof data.response.choices[0].text === "string") {
+          setRecipe(data.response.choices[0].text)
+          //setPickupArray(pickupArray.concat(<h2>{data.response.choices[0].text}</h2>))
+          
+        }
+        setLoading(false)
+      })
+      .catch((err) => {
+        console.log(err);
+        setLoading(false);
+      })
+
+        
+    }
+    return (
+    <div className='container mx-auto border mt-5 min-h-96 border-zinc-700'>
+      <div className='grid grid-cols-2 gap-4'>
+        <div className='text-center py-32'>
+          <button disabled={loading} onClick={handleClick} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:bg-slate-200 disabled:cursor-wait" >
+          Generate Dish
+          </button>
+            {/* <div className='py-12 text-xl'>{pickup}</div> */}
+        </div>
+        <div>
+            <div className='text-center'>{recipe}</div>
+        </div>
+      </div>
+    </div>
+    )
+}
