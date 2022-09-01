d1143419e69433dd1443a2278e1051167c582655
Daniel Mathisen Breivik <danbre14@nfk.no>
2022-09-01 13:20:47 +0200

    search styling

--- pages/danielsrecipe.tsx
+++ pages/danielsrecipe.tsx
@@ -1,10 +1,12 @@
 import { openai, openAi } from '../lib/server/openai';
 import React, { useState } from 'react';
 import { ResponseData } from './api/openai/recipeDaniel';
+import { toast } from 'react-toastify';
 let WEB_URL = 'http://localhost:3000/';
 
 export default function Danielsquote() {
   const [generatedLines, setGeneratedLines] = useState<string[]>([]);
+  const [selectedGeneratedLine, setSelectedGeneratedLine] = useState<number>(0);
   const [availableIngredients, setAvailableIngredients] = useState<string[]>([
     'egg',
     'flour',
@@ -12,13 +14,11 @@ export default function Danielsquote() {
     'milk',
   ]);
   const [selectedIngredients, setSelectedIngredients] = useState<string[]>([]);
-  const [selectedIngredient, setSelectedIngredient] = useState<string>('');
+  const [searchSelection, setSearchSelection] = useState<number>(0);
   const [loading, setLoading] = useState<boolean>(false);
   const [error, setError] = useState<string>('');
-  const [recentChange, setRecentChange] = useState<number>(-1);
   const [searchText, setSearchText] = useState<string>('');
   const [searchResults, setSearchResults] = useState<string[]>([]);
-  const [hidableContent, setHidableContent] = useState<boolean>(true);
   let selectableIngredients = availableIngredients.filter(
     (ingredient) => !selectedIngredients.includes(ingredient),
   );
@@ -66,10 +66,28 @@ export default function Danielsquote() {
       });
     }
   };
-  const handleEnterPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
+  const handleKeyPressUp = (event: React.KeyboardEvent<HTMLInputElement>) => {
     if (event.key === 'Enter') {
       setSearchText('');
-      addNewIngredient(event.currentTarget.value);
+      if (searchResults[searchSelection]) {
+        addNewIngredient(searchResults[searchSelection]);
+      }
+    }
+  };
+  const handleKeyPressDown = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'ArrowDown') {
+      if (searchResults[searchSelection + 1]) {
+        setSearchSelection(searchSelection + 1);
+      } else {
+        setSearchSelection(0);
+      }
+    }
+    if (event.key === 'ArrowUp') {
+      if (searchResults[searchSelection - 1]) {
+        setSearchSelection(searchSelection - 1);
+      } else {
+        setSearchSelection(searchResults.length - 1);
+      }
     }
   };
   const search = (searchTerm: string) => {
@@ -80,6 +98,9 @@ export default function Danielsquote() {
       .filter((ingredient: string) => {
         return !selectedIngredients.includes(ingredient);
       });
+
+    if (!existsInArrayLowerCase(results, searchTerm))
+      results.unshift(searchTerm);
     setSearchResults(results);
     return false;
   };
@@ -92,8 +113,10 @@ export default function Danielsquote() {
   const generateRecipe = (): void | boolean => {
     if (loading === true) return false;
     const apiUrl = new URL(WEB_URL + 'api/openai/recipeDaniel');
-    if (selectedIngredients.length === 0)
+    if (selectedIngredients.length === 0) {
+      toast.error('No ingredient(s) selected...');
       return setError('No ingredient(s) selected...');
+    }
     setLoading(true);
 
     for (let o of selectedIngredients) {
@@ -120,14 +143,21 @@ export default function Danielsquote() {
   };
 
   return (
-    <div className="m-auto w-max">
-      <div className="m-0 w-52 h-52">
-        <div className="p-0 min-w-max m-0 block text-center self-center">
+    // <div className="w-screen bg-green-50">
+    //   <div className="w-max float-left bg-red-300">left side</div>
+    //   <div className="w-max float-right bg-blue-200">right side</div>
+    // </div>
+    <div className="w-screen">
+      <div className=" h-52 w-max mb-10 w-">
+        <div
+          className="p-0  w-60 min-w-max m-auto block text-center self-center float-left
+        "
+        >
           {selectedIngredients.map((ingredient, i) => {
             return (
               <div
                 key={i}
-                className="w-auto cursor-pointer border content-center m-1"
+                className="w- cursor-pointer border content-center m-1 "
               >
                 <label
                   className=" cursor-pointer
@@ -150,73 +180,81 @@ export default function Danielsquote() {
           })}
         </div>
 
-        <input
-          className="border w-auto   m-1"
-          placeholder={`Custom/search ingredient `}
-          type={'text'}
-          onInput={customInput}
-          value={searchText}
-          onKeyUp={handleEnterPress}
-        />
-        <div>
-          {searchText.length > 0 && <p>Search results for {searchText}:</p>}
-          <div>
-            {searchResults.map((result, id) => {
-              return (
-                <div
-                  onClick={() => {
-                    addNewIngredient(result);
-                  }}
-                  key={id}
-                >
-                  {result}
-                </div>
-              );
-            })}
+        <div className="w-60 float-right">
+          <input
+            className="border w-60 rounded text-center   m-1"
+            placeholder={`Custom/search ingredient `}
+            type={'text'}
+            onInput={customInput}
+            value={searchText}
+            onKeyDown={handleKeyPressDown}
+            onKeyUp={handleKeyPressUp}
+          />
+
+          <div className="relative">
+            <div className="aboslute bg-white w-60 border border-gray-300 text-center rounded-lg flex flex-col text-sm py-2 px-2 text-gray-500 shadow-lg">
+              {searchResults.map((result, id) => {
+                let isSelected = id === searchSelection ? 'bg-blue-100' : '';
+                return (
+                  <div
+                    className={'hover:bg-gray-200 mt-1 rounded ' + isSelected}
+                    onClick={() => {
+                      addNewIngredient(result);
+                    }}
+                    key={id}
+                  >
+                    {result}
+                  </div>
+                );
+              })}
+            </div>
           </div>
         </div>
       </div>
-      <div className="mt-10">
+      <div className="mt-20 float-right text-center w-screen">
         {error !== '' && (
           <div className="text-red-500">
             <label>{error}</label>
           </div>
         )}
+        {generatedLines.reverse()[selectedGeneratedLine - 1] !== undefined && (
+          <button
+            onClick={() => {
+              setSelectedGeneratedLine(selectedGeneratedLine - 1);
+            }}
+            className={
+              'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:bg-slate-200 disabled:cursor-wait '
+            }
+            disabled={loading}
+          >
+            prev
+          </button>
+        )}
         <button
           onClick={generateRecipe}
           className={
-            'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:bg-slate-200 disabled:cursor-wait '
+            'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-5 disabled:bg-slate-200 disabled:cursor-wait '
           }
           disabled={loading}
         >
           Generate
         </button>
-        <div className="whitespace-pre-wrap">{generatedLines.reverse()[0]}</div>
-
-        {generatedLines.length > 1 && (
-          <div className="mt-20">
-            {!hidableContent && (
-              <div>
-                {generatedLines.slice(1).map((line, i) => {
-                  return (
-                    <div key={i} className="whitespace-pre-wrap">
-                      {line}
-                    </div>
-                  );
-                })}
-              </div>
-            )}
-            <button
-              onClick={() => {
-                setHidableContent(!hidableContent);
-              }}
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-0.5 px-4 min-w-full rounded"
-            >
-              {hidableContent && <p>Show</p>}
-              {!hidableContent && <p>hide</p>}
-            </button>
-          </div>
+        {generatedLines.reverse()[selectedGeneratedLine + 1] !== undefined && (
+          <button
+            onClick={() => {
+              setSelectedGeneratedLine(selectedGeneratedLine + 1);
+            }}
+            className={
+              'bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded disabled:bg-slate-200 disabled:cursor-wait '
+            }
+            disabled={loading}
+          >
+            next
+          </button>
         )}
+        <div className="whitespace-pre-wrap text-center ">
+          {generatedLines.reverse()[selectedGeneratedLine]}
+        </div>
       </div>
     </div>
   );
